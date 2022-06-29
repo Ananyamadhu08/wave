a86a38a82b467ed2cf49612a326ba17b2e0abf84
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-06-29 21:37:54 +0530

    [FEAT]: created user controller

---
+++ controllers/userController.js
@@ -0,0 +1,168 @@
+const Users = require('../models/userModel');
+
+const userController = {
+  searchUser: async (req, res) => {
+    try {
+      const users = await Users.find({
+        username: { $regex: req.query.username },
+      })
+        .limit(10)
+        .select('fullName userName avatar');
+
+      res.json({ users });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+
+  getUser: async (req, res) => {
+    try {
+      const user = await Users.findById(req.params.id).select('-password');
+
+      if (!user) return res.status(400).json({ msg: 'User not found.' });
+
+      res.json({ user });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+
+  updateUser: async (req, res) => {
+    try {
+      const {
+        avatar,
+        userName,
+        fullName,
+        mobile,
+        address,
+        story,
+        website,
+        gender,
+      } = req.body;
+
+      if (!fullName)
+        return res.status(400).json({ msg: 'Please add your full name.' });
+
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          avatar,
+          userName,
+          fullName,
+          mobile,
+          address,
+          story,
+          website,
+          gender,
+        }
+      );
+      res.json({ msg: 'Updated profile details successfully.' });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+
+  follow: async (req, res) => {
+    try {
+      const user = await Users.find({
+        _id: req.params.id,
+        followers: req.user._id, // this is to check if the user is already being followed
+      });
+
+      if (user.length > 0)
+        return res
+          .status(500)
+          .json({ msg: 'You are already following this user.' });
+
+      // this is to find the user to follow and update the user's followers with your id
+      const newUser = await Users.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $push: { followers: req.user._id },
+        },
+        {
+          new: true,
+        }
+      );
+
+      // this is to update in the user's following the id of the user just followed
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $push: { following: req.params.id },
+        },
+        {
+          new: true,
+        }
+      );
+
+      return res.json({ newUser });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+
+  unfollow: async (req, res) => {
+    try {
+      // this is to remove the user id from the followers of the person who has been unfollowed
+      const newUser = await Users.findOneAndUpdate(
+        { _id: req.params.id },
+        {
+          $pull: { followers: req.user._id },
+        },
+        { new: true }
+      ).populate('followers following', '-password');
+
+      // this is to remove the user id of the user who has just been unfollowed in user's following
+      await Users.findOneAndUpdate(
+        { _id: req.user._id },
+        {
+          $pull: { following: req.params.id },
+        },
+        { new: true }
+      );
+
+      res.json({ newUser });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+
+  suggestedUsers: async (req, res) => {
+    try {
+      const newArr = [...req.user.following, req.user._id];
+
+      const num = req.query.num || 10;
+
+      const users = await Users.aggregate([
+        { $match: { _id: { $nin: newArr } } },
+        { $sample: { size: Number(num) } },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'followers',
+            foreignField: '_id',
+            as: 'followers',
+          },
+        },
+        {
+          $lookup: {
+            from: 'users',
+            localField: 'following',
+            foreignField: '_id',
+            as: 'following',
+          },
+        },
+      ]).project('-password');
+
+      return res.json({
+        users,
+        result: users.length,
+      });
+    } catch (error) {
+      return res.status(500).json({ msg: error.message });
+    }
+  },
+};
+
+module.exports = userController;
