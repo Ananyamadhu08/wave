44e1ab572ec8f5b65fd92bfb4576376e63abd3c1
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-07-15 18:29:58 +0700

    [FEAT]: created routes and page renderer component

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,18 @@
-function App() {
-  return <div className="App">hello world</div>;
-}
+import { Route, Routes } from "react-router-dom";
+import { PageRenderer } from "./customRouter";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/:page" element={<PageRenderer />} />
+
+      <Route path="/:page/:id" element={<PageRenderer />} />
+    </Routes>
+  );
+};
 
 export default App;
