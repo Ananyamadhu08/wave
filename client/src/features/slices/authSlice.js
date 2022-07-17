211724d7d1c946902c2c5d1e4f8e44227361a72a
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-07-17 13:37:25 +0700

    [FEAT]: created auth slice

---
+++ client/src/features/slices/authSlice.js
@@ -0,0 +1,57 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  loginUser,
+  logoutUser,
+  refreshToken,
+  registerUser,
+} from "../actions/authActions";
+
+const initialState = {
+  token: null,
+  user: null,
+};
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setAuth: (state, { payload }) => {
+      state.token = payload.token;
+      state.user = payload.user;
+    },
+  },
+  extraReducers: {
+    [loginUser.fulfilled]: (state, { payload }) => {
+      state.user = payload.user;
+      state.token = payload.access_token;
+    },
+    [loginUser.rejected]: (state, { payload }) => {
+      state.loading = false;
+    },
+    [refreshToken.fulfilled]: (state, { payload }) => {
+      state.user = payload.user;
+      state.token = payload.access_token;
+    },
+    [refreshToken.rejected]: (state, { payload }) => {
+      state.loading = false;
+    },
+    [registerUser.fulfilled]: (state, { payload }) => {
+      state.user = payload.user;
+      state.token = payload.access_token;
+    },
+    [registerUser.rejected]: (state, { payload }) => {
+      state.loading = false;
+    },
+    [logoutUser.fulfilled]: (state, { payload }) => {
+      state.user = null;
+      state.token = null;
+    },
+    [logoutUser.rejected]: (state, { payload }) => {
+      state.loading = false;
+    },
+  },
+});
+
+const { reducer, actions } = authSlice;
+export const { setAuth } = actions;
+export default reducer;
