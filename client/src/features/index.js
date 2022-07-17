2b245d85de2cbee1a06902d61620b334037a14fb
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-07-17 13:38:39 +0700

    [FEAT]: setup redux store

---
+++ client/src/features/index.js
@@ -0,0 +1,12 @@
+// reducers
+
+// actions
+import {
+  loginUser,
+  refreshToken,
+  registerUser,
+  logoutUser,
+} from "./actions/authActions";
+
+// exports
+export { loginUser, refreshToken, registerUser, logoutUser };
---
+++ client/src/utils/index.js
@@ -0,0 +1,9 @@
+import {
+  getDataAPI,
+  postDataAPI,
+  putDataAPI,
+  patchDataAPI,
+  deleteDataAPI,
+} from "./fetchData";
+
+export { getDataAPI, postDataAPI, putDataAPI, patchDataAPI, deleteDataAPI };
