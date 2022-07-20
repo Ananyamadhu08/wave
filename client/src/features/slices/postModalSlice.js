20f2c01568277a700db472f012b8d941cc18462a
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-07-20 18:49:28 +0700

    [FEAT]: created post modal slice

---
+++ client/src/features/slices/postModalSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  isModalOpen: false,
+  onEdit: false,
+  editPost: {},
+};
+
+const postModalSlice = createSlice({
+  name: "postModal",
+  initialState,
+  reducers: {
+    setPostModal: (state, { payload }) => {
+      state.isModalOpen = payload.isModalOpen;
+    },
+    setEditPostModal: (state, { payload }) => {
+      state.isModalOpen = payload.isModalOpen;
+      state.onEdit = payload.onEdit;
+      state.editPost = { ...payload.editPost };
+    },
+  },
+});
+
+const { reducer, actions } = postModalSlice;
+export const { setPostModal, setEditPostModal } = actions;
+export default reducer;
