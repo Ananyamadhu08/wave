b0c1c2040cf8c0b249cfdf6bd73b54a0df86ae5c
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-07-22 20:47:09 +0700

    [FIX]: deployment fix

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'hello world' });
-});
+// app.get('/', (req, res) => {
+//   res.json({ msg: 'hello world' });
+// });
 
 const URI = process.env.MONGODB_URL;
 
@@ -22,10 +22,6 @@ mongoose.connect(URI, (err) => {
   if (err) throw err;
   console.log('connected to mongoDB');
 });
-
-// app.get('/', (req, res) => {
-//   res.send('Backend');
-// });
 
 // routes
 app.use('/api', require('./routes/authRouter'));
